97128ee7ce403bc4bda8160349c4d992cb7142f6
dairan <dairan@winning.com.cn>
2020-05-06 16:42:40 +0800

    卡壳在如何exec 执行gitbash

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { workspace as ws } from 'vscode';
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
-
 /**
  * 获取当前所在工程根目录，有3种使用方法：<br>
  * getProjectPath(uri) uri 表示工程内某个文件的路径<br>
@@ -13,45 +12,9 @@ const path = require('path');
  * getProjectPath() 会自动从 activeTextEditor 拿document对象，如果没有拿到则报错
  * @param {*} document 
  */
-const ad = getProjectPath(null)
-function getProjectPath(document: any) {
-	debugger
-	if (!document) {
-			document = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.document : null;
-	}
-	if (!document) {
-			vscode.window.showErrorMessage('当前激活的编辑器不是文件或者没有文件被打开！');
-			return '';
-	}
-	const currentFile = (document.uri ? document.uri : document).fsPath;
-	let projectPath = null;
-	let abc = ws.workspaceFolders
-  if (!abc) {
-		abc = []
-	}
-	let workspaceFolders = abc.map(item => item.uri.path);
-	// 由于存在Multi-root工作区，暂时没有特别好的判断方法，先这样粗暴判断
-	// 如果发现只有一个根文件夹，读取其子文件夹作为 workspaceFolders
-	if (workspaceFolders.length == 1 && workspaceFolders[0] === vscode.workspace.rootPath) {
-			const rootPath = workspaceFolders[0];
-			var files = fs.readdirSync(rootPath);
-			workspaceFolders = files.filter((name:any) => !/^\./g.test(name)).map((name: any) => path.resolve(rootPath, name));
-			// vscode.workspace.rootPath会不准确，且已过时
-			// return vscode.workspace.rootPath + '/' + this._getProjectName(vscode, document);
-	}
-	workspaceFolders.forEach(folder => {
-			if (currentFile.indexOf(folder) === 0) {
-					projectPath = folder;
-			}
-	})
-	if (!projectPath) {
-			vscode.window.showErrorMessage('获取工程根路径异常！');
-			return '';
-	}
-	projectPath = path.resolve(projectPath, '../')
-	return projectPath;
-}
+let rootPath = vscode.workspace.rootPath
 
+debugger
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -62,14 +25,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('totalcodeline.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('totalcodeline.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
     
 		// Display a message box to the user
-		vscode.window.showInformationMessage(ad);
-		exec('git config user.name', { cwd: ad }, (data: Object) => {
-			console.log(data, 'acb')
-			debugger
+		vscode.window.showInformationMessage(rootPath || '获取路径错误');
+		let a = await exec('git config user.name', { cwd: rootPath }, (err: any, stdout:any, stderr: any) => {
+			const codeStatisCmd = `git log --author=${stdout} --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -`
+			if (!err) {
+				exec(codeStatisCmd, (err:any, stdout:any,stderr: any) => {
+					console.log(stdout, 'abc')
+				})
+			}
 		})
 	});
 
